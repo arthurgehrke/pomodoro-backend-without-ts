5a987bd28bfe99b9f708159979402baaacbb0751
arthurgehrke <arthurgehrke07@gmail.com>
2020-07-05 16:36:44 -0300

    feat: start hourHistoryController

---
+++ src/app/controllers/HourHistoryController.js
@@ -0,0 +1,11 @@
+const HourHistory = require('../models/HourHistory')
+
+class HourHistoryController {
+	async store(req, res) {
+		const { minutes } = req.body
+		try {
+		} catch (err) {}
+	}
+}
+
+module.exports = new HourHistoryController()
